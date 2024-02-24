00baaff708a09b66d77a6084e73e85ac6e0ca96c
Ada <ada@gnous.eu>
2024-02-25 00:29:39 +0100

    lock: Add secret token hashing with argon2id

--- internal/constant/constants.go
+++ internal/constant/constants.go
@@ -5,4 +5,10 @@ import "time"
 const (
 	HTTPTimeout          = 3 * time.Second
 	ExpirationCurlCreate = 604800 * time.Second // Second in one week
+	TokenLength          = 32
+	ArgonSaltSize        = 16
+	ArgonMemory          = 64 * 1024
+	ArgonThreads         = 4
+	ArgonKeyLength       = 32
+	ArgonIterations      = 2
 )
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,8 +1,6 @@
 package utils
 
 import (
-	"crypto/rand"
-	"encoding/hex"
 	"log"
 	mathrand "math/rand/v2"
 	"regexp"
@@ -21,17 +19,6 @@ func GenerateUrl(length uint8) string {
 	return string(b)
 }
 
-// GenerateSecret generate random secret (32 bytes hexadecimal)
-func GenerateSecret() string {
-	key := make([]byte, 32)
-	_, err := rand.Read(key)
-	if err != nil {
-		log.Printf("Failed to generate secret")
-	}
-
-	return hex.EncodeToString(key)
-}
-
 // CheckCharRedundant  verify is a character is redundant in a string
 func CheckCharRedundant(source string, char string) bool { // Verify if a char is redundant
 	return strings.Count(source, char) > 1
@@ -39,7 +26,7 @@ func CheckCharRedundant(source string, char string) bool { // Verify if a char i
 
 func parseIntBeforeSeparator(source *string, sep string) (int, error) { // return 0 & error if error, only accept positive number
 	if CheckCharRedundant(*source, sep) {
-		return 0, &ParseIntBeforeSeparatorError{Message: *source + ": cannot parse value as int"}
+		return 0, &parseIntBeforeSeparatorError{message: *source + ": cannot parse value as int"}
 	}
 	var value int
 	var err error
@@ -47,14 +34,14 @@ func parseIntBeforeSeparator(source *string, sep string) (int, error) { // retur
 		value, err = strconv.Atoi(strings.Split(*source, sep)[0])
 		if err != nil {
 			log.Println(err)
-			return 0, &ParseIntBeforeSeparatorError{Message: *source + ": cannot parse value as int"}
+			return 0, &parseIntBeforeSeparatorError{message: *source + ": cannot parse value as int"}
 		}
 		if value < 0 { // Only positive value is correct
-			return 0, &ParseIntBeforeSeparatorError{Message: *source + ": format only take positive value"}
+			return 0, &parseIntBeforeSeparatorError{message: *source + ": format only take positive value"}
 		}
 
 		if value > 99 {
-			return 0, &ParseIntBeforeSeparatorError{Message: *source + ": Format only take two number"}
+			return 0, &parseIntBeforeSeparatorError{message: *source + ": Format only take two number"}
 		}
 
 		*source = strings.Join(strings.Split(*source, sep)[1:], "")
@@ -77,25 +64,25 @@ func ParseExpiration(source string) (int, error) {
 	expiration = tempOutput * 86400
 	if err != nil {
 		log.Println(err)
-		return 0, &ParseExpirationError{Message: "Invalid syntax"}
+		return 0, &ParseExpirationError{message: "Invalid syntax"}
 	}
 	tempOutput, err = parseIntBeforeSeparator(&source, "h")
 	expiration += tempOutput * 3600
 	if err != nil {
 		log.Println(err)
-		return 0, &ParseExpirationError{Message: "Invalid syntax"}
+		return 0, &ParseExpirationError{message: "Invalid syntax"}
 	}
 	tempOutput, err = parseIntBeforeSeparator(&source, "m")
 	expiration += tempOutput * 60
 	if err != nil {
 		log.Println(err)
-		return 0, &ParseExpirationError{Message: "Invalid syntax"}
+		return 0, &ParseExpirationError{message: "Invalid syntax"}
 	}
 	tempOutput, err = parseIntBeforeSeparator(&source, "s")
 	expiration += tempOutput * 1
 	if err != nil {
 		log.Println(err)
-		return 0, &ParseExpirationError{Message: "Invalid syntax"}
+		return 0, &ParseExpirationError{message: "Invalid syntax"}
 	}
 
 	return expiration, nil
