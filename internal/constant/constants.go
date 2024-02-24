8968046b110f45da7bc655df71fb6cc90167585a
Ada <ada@gnous.eu>
2024-02-24 01:26:27 +0100

    :sparkles: Implement basic POST request for curl
    / can now read body and create paste, for curl like client, web create with full settings is move to /create

--- internal/constant/constants.go
+++ internal/constant/constants.go
@@ -3,5 +3,6 @@ package constant
 import "time"
 
 const (
-	HTTPTimeout = 3 * time.Second
+	HTTPTimeout          = 3 * time.Second
+	ExpirationCurlCreate = 604800 * time.Second // Second in one week
 )
